bf8f38aacd4e8e6f847a9ca4358e8ad5bf89957b
MrPig91 <sirius.cleveland@gmail.com>
2021-07-16 21:29:38 -0500

    New Public Function Get-fpBlocksChart

---
+++ PSFlexPool/Public/Get-fpBlocksChart.ps1
@@ -0,0 +1,30 @@
+function Get-fpBlocksChart {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [ValidateSet("XCH","ETH")]
+        [string[]]$CoinTicker = @("XCH","ETH")
+    )
+
+    Process{
+        foreach ($coin in $CoinTicker){
+            try{
+                $Query = "pool/blocksChart?coin=$coin"
+                $Results = Invoke-FlexPoolAPI -Query $Query
+                if ($null -eq $Results.error){
+                    foreach ($blockchart in $Results.result){
+                        $blockchart.psobject.TypeNames.Insert(0,"PSFlexPool.BlockChart")
+                        $blockchart | Add-Member -MemberType NoteProperty -Name Coin -Value $coin
+                        $blockchart
+                    }
+                }
+                else{
+                    Write-Error $Results.error -ErrorAction Stop
+                }
+            }
+            catch{
+                $PSCmdlet.WriteError($_)
+            } #try/catch
+        } #foreach coin
+    } #Process
+}
