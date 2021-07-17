c24f3fa0c3a8185c359c9e23b1fc7af2b5482e1e
MrPig91 <sirius.cleveland@gmail.com>
2021-07-16 19:43:42 -0500

    New Public Function Get-fpDailyRewardPerGigahashSec

---
+++ PSFlexPool/Public/Get-fpDailyRewardPerGigahashSec.ps1
@@ -0,0 +1,29 @@
+function Get-fpDailyRewardPerGigahashSec {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [ValidateSet("XCH","ETH")]
+        [string[]]$CoinTicker = @("XCH","ETH")
+    )
+
+    Process{
+        foreach ($coin in $CoinTicker){
+            try{
+                $Query = "pool/dailyRewardPerGigahashSec?coin=$coin"
+                $Results = Invoke-FlexPoolAPI -Query $Query
+                if ($null -eq $Results.error){
+                    [pscustomobject]@{
+                        Coin = $coin
+                        DailyRewardPerGigahashSec = [int64]$Results.Result
+                    }
+                }
+                else{
+                    Write-Error $Results.error -ErrorAction Stop
+                }
+            }
+            catch{
+                $PSCmdlet.WriteError($_)
+            } #try/catch
+        } #foreach coin
+    } #Process
+}
