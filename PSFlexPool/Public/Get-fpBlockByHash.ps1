e0c0b681a828bde77587911a4cf677024cbbdaf0
MrPig91 <sirius.cleveland@gmail.com>
2021-07-16 21:29:28 -0500

    New Public Function Get-fpBlockByHash

---
+++ PSFlexPool/Public/Get-fpBlockByHash.ps1
@@ -0,0 +1,30 @@
+function Get-fpBlockByHash {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [ValidateSet("XCH","ETH")]
+        [string]$CoinTicker = "xch",
+
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [Alias("Hash")]
+        [string]$Blockhash
+    )
+
+    Process{
+        try{
+            $Query = "pool/blockByHash?coin=$CoinTicker&blockHash=$BlockHash"
+            $Results = Invoke-FlexPoolAPI -Query $Query
+            if ($null -eq $Results.error){
+                $Results.result.psobject.TypeNames.Insert(0,"PSFlexPool.Block")
+                $Results.result
+            }
+            else{
+                Write-Error $Results.error -ErrorAction Stop
+            }
+        }
+        catch{
+            $PSCmdlet.WriteError($_)
+        } #try/catch
+    } #Process
+}
