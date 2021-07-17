a955e7550fc425e9457c530e5b764e7ab6b83fcb
MrPig91 <sirius.cleveland@gmail.com>
2021-07-17 12:30:16 -0500

    New Public Function Get-fpMinerBlock

---
+++ PSFlexPool/Public/Get-fpMinerBlock.ps1
@@ -0,0 +1,40 @@
+function Get-fpMinerBlock {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [ValidateSet("XCH","ETH")]
+        [string]$CoinTicker,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName,ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Address,
+        [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [int]$Page = 0
+    )
+
+    Process{
+        try{
+            $Query = "miner/blocks?coin=$CoinTicker&address=$Address&page=$Page"
+            $Results = Invoke-FlexPoolAPI -Query $Query
+            return $Results
+            if ($null -eq $Results.error){
+                $Results.result.data | ForEach-Object {$_.psobject.TypeNames.Insert(0,"PSFlexPool.MinerBlock")}
+                [PSCustomObject]@{
+                    PSTypeName = "PSFlexPool.MinerBlockPage"
+                    TotalItems = $Results.result.TotalItems
+                    TotalPages = $Results.result.TotalPages
+                    CurrentPage = $Page
+                    Blocks = $Results.result.data
+                    Coin = $CoinTicker
+                    Address = $Address
+                }
+            }
+            else{
+                Write-Error $Results.error -ErrorAction Stop
+            }
+        }
+        catch{
+            $PSCmdlet.WriteError($_)
+        } #try/catch
+    } #Process
+}
