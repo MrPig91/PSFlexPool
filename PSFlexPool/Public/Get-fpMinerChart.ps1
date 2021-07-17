a3d49e33e904f7013da0bc7cb6cf92ed5fd5eb38
MrPig91 <sirius.cleveland@gmail.com>
2021-07-17 12:30:38 -0500

    New Public Function Get-fpMinerChart

---
+++ PSFlexPool/Public/Get-fpMinerChart.ps1
@@ -0,0 +1,32 @@
+function Get-fpMinerChart {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [ValidateSet("XCH","ETH")]
+        [string]$CoinTicker,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName,ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Address
+    )
+
+    Process{
+        try{
+            $Query = "miner/chart?coin=$CoinTicker&address=$Address"
+            $Results = Invoke-FlexPoolAPI -Query $Query
+            if ($null -eq $Results.error){
+                $Results.result | Foreach-Object {$_.psobject.TypeNames.insert(0,"PSFlexPool.MinerChart")}
+                $Results.result | Add-Member -NotePropertyMembers @{
+                    Coin = $CoinTicker
+                    Address = $Address
+                }
+                $Results.result
+            }
+            else{
+                Write-Error $Results.error -ErrorAction Stop
+            }
+        }
+        catch{
+            $PSCmdlet.WriteError($_)
+        } #try/catch
+    } #Process
+}
